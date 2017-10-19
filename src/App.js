98e20cee29c7dbea1216165d1dbb4dd8f976bfcc
github username <github email address>
2017-10-19 18:08:35 -0400

    added personality container, signup + login forms; routed all

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import PersonalityContainer from './components/PersonalityContainer'
+import { Button } from 'semantic-ui-react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import SignUpForm from './components/SignUpForm'
+import LoginForm from './components/LoginForm'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title" >Personality Insights</h1>
+      <p className="App-intro">
+        <PersonalityContainer/>
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/signup" component={SignUpForm} />
+      </p>
       </div>
     );
   }
 }
+
 
 export default App;
